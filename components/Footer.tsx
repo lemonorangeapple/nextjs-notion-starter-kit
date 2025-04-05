f2fd70e502e3d2e711a7b5cb0f4bac92934a7027
June <107529938+lemonorangeapple@users.noreply.github.com>
2025-04-05 16:18:45 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -53,16 +53,7 @@ export const FooterImpl: React.FC = () => {
       <div id="hengping">
         <h1>请使用横屏观看</h1>
       </div>
-      <script>
-      window.addEventListener("resize", (event) => {
-	      const orientation = (window.innerWidth > window.innerHeight) ? "landscape" : "portrait";
-	      if (orientation === 'portrait') {
-		      document.getElementById("hengping").style.display = "contents";
-	      } else {
-		      document.getElementById("hengping").style.display = "none";
-	      }
-      }, false);
-      </script>
+      
     <footer className={styles.footer}>
       <div className={styles.copyright}>Copyright 2025 {config.author}</div>
 
